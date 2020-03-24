53e5e004ccb44ae73014c4c1a367ec2974f62b3f
Henry Nelson <Henry.Nelson89@gmail.com>
2020-03-24 16:00:08 -0700

    met MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './Form'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
+
+
 function App() {
+  const [teamMembers, setTeamMembers]= useState([])
+  const [formValues, setFormValues]= useState({
+    fname: '',
+    lname: '',
+    role: '',
+  })
+
+  const onInputChange = event => {
+      const inputThatChanged = event.target.name
+      const newValueForInput = event.target.value
+     
+     setFormValues({
+      ...formValues,
+      [inputThatChanged]: newValueForInput,
+     })
+  
+    
+   }
+   const onFormSubmit = event => {
+      event.preventDefault()
+    
+      const newMember = {
+        fname: formValues.fname,
+        lname: formValues.lname,
+        role: formValues.role,
+        id: uuidv4
+      }
+      setTeamMembers([...teamMembers, newMember])
+  }
+
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+          onInputChange={onInputChange}
+          formValues={formValues}
+          onFormSubmit={onFormSubmit}
+      
+      />
+     <h3>Here is the team!:</h3>
+      {
+        teamMembers.map(fr => <div key={fr.id}>{fr.fname} {fr.lname}</div>)
+      }
     </div>
   );
-}
+  }
 
 export default App;
